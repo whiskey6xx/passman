8270372d880d56e32635511e73b9ba611f33e177
whiskey6xx <87744468+whiskey6xx@users.noreply.github.com>
2023-04-05 10:02:28 -0400

    Update passman.py
    cleaned up ui, added random password generation, streamlined main menu

--- passman.py
+++ passman.py
@@ -3,22 +3,20 @@
 from pmdbman import *
 from pmcryptman import *
 import os
+import random
+import string
 
 menuMessage = '''
 # WELCOME TO THE PASSMAN
 # Please select an option:
 ---------------------------------------------
-#1 Show all site entries
-#2 Retrieve credentials
-#3 Create new entry
-#4 Update entry 
-#5 Delete entry
-#6 Exit Passman
+#1 Retrieve credentials
+#2 Create new entry
+#3 Update entry 
+#4 Delete entry
+#5 Exit Passman
 ---------------------------------------------
 '''
-
-
-
 
 def go_back():
     while True:
@@ -33,34 +31,60 @@ def go_back():
 def ui_clear():
     os.system('clear')
 
-def show_all_entries():
-    while True:
+def show_all_entries():    
+    db_showall()
         
-        db_showall()
         
-        goBack = go_back()
-        if goBack == True:
-            break
 
 def get_password():
     while True:
+        try:
+            show_all_entries()
+            i = input("# Please enter the name of the site:")
+
+            x = db_search(i)
+
+            print("# The Username is " + x[0] + "\n# The Password is " + x[1])
         
-        i = input("# Please enter the name of the site:")
-
-        x = db_search(i)
-        print(x[0] + ":" + x[1])
-
-        goBack = go_back()
-        if goBack == True:
+            goBack = go_back()
+            
+            if goBack == True:
+                break
+            else:
+                ui_clear()
+        except TypeError:
+            print("# Entry does not exist")
             break
+
+def generate_password():
+    letters = string.ascii_letters # all letters
+    digits = string.digits # all digits
+    special_chars = string.punctuation # all special characters
+    
+    password = []
+    password.extend(random.choices(letters, k=8))
+    password.extend(random.choices(digits, k=4))
+    password.append(random.choice(special_chars))
+    
+    # shuffle password
+    random.shuffle(password)
+    
+    # return as string
+    return ''.join(password)
 
 def make_a_entry():
     while True:
         
         i2 = input("# Please enter the site name:")
         i3 = input("# Please enter the Username:")
-        i4 = input("# Please enter the password:")
-        
+        choice = input("# Generate a random pw?(y/n):")
+        if choice == "y":
+            i4 = generate_password()
+        elif choice == "n":
+            i4 = input("# Please enter the password:")
+        else:
+            print("#Wrong input")
+            continue
         db_insert(i2, i3, i4)
 
         print("# Account for " + str(i2) + " has been inserted!")
@@ -68,32 +92,37 @@ def make_a_entry():
         goBack = go_back()
         if goBack == True:
             break
-
+        else:
+            ui_clear()
 
 def update():
     while True:
-        
-        i = int(input("# Please enter the ID of the site:"))
-        i2 = input("# Please enter the password:")
+        show_all_entries()
+        i = input("# Please enter the name of the site:")
+        i2 = input("# Please enter the new password:")
 
         db_update(i, i2)
 
-        print("# Password has been updated for ID " + str(i))
+        print("# Password has been updated for " + str(i))
         
         goBack = go_back()
         if goBack == True:
             break
+        else:
+            ui_clear()
 
 def delete_entry():
     while True:
-        
-        i = int(input("# Please enter the ID of the entry you wish the remove:"))
+        show_all_entries()
+        i = input("# Please enter the name of the entry you wish the remove:")
         db_delete(i)
-        print("# Successfully deleted!")
+        
         
         goBack = go_back()
         if goBack == True:
             break
+        else:
+            ui_clear()
 
 
 def main():
@@ -105,32 +134,29 @@ def main():
 
             option = int(input("#ENTER THE NUMBER OF YOUR SELECTION:"))
                 
-            if option == 1:
+               
+            if option == 1: #Get password
                 #clear here
                 ui_clear()
-                show_all_entries()
-
-            elif option == 2: #Get password
-                #clear here
-                ui_clear()
+                
                 get_password()
             
-            elif option == 3: #Make an entry
+            elif option == 2: #Make an entry
                 #clear here
                 ui_clear()
                 make_a_entry()
 
-            elif option == 4: #Update the password for an entry
+            elif option == 3: #Update the password for an entry
                 #clear here
                 ui_clear()
                 update()
 
-            elif option == 5: #delete an entry
+            elif option == 4: #delete an entry
                 #clear here
                 ui_clear()
                 delete_entry()        
 
-            elif option == 6: #exit
+            elif option == 5: #exit
                 #clear here
                 ui_clear()
                 break
@@ -138,7 +164,7 @@ def main():
             else:
                 print("# INVALID INPUT")
         except ValueError:
-                print("# MUST ENTER AN INTEGER!")
+                print("# VALUE ERROR!")
 
 
 def passman_start():
